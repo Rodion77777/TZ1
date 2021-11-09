3f9aef47173ab8495c76901efcd3f2a5b8def274
Rodion <rodionchik17@gmail.com>
2021-11-09 17:49:12 +0200

    mini changes in ProductController

--- src/main/java/company/api/controllers/ProductController.java
+++ src/main/java/company/api/controllers/ProductController.java
@@ -2,16 +2,18 @@ package company.api.controllers;
 
 import company.business.DTO.ProductDTO;
 import company.business.services.ProductService;
-import company.domain.entity.ProductEntity;
-import company.domain.managers.ShowManager;
+import company.domain.managers.DTOManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static java.lang.System.out;
+
 @RestController
 public class ProductController {
     private ProductService productService;
+    private String marker = "*\n".repeat(50);
 
     public ProductController (ProductService productService)
     {
@@ -21,6 +23,7 @@ public class ProductController {
     @GetMapping("/getAllProducts")
     public List<ProductDTO> getAllProducts ()
     {
+        out.println(marker);
         return productService.getAllProducts();
     }
 
@@ -28,14 +31,24 @@ public class ProductController {
     @GetMapping("/getProductDTO")
     public ProductDTO getProductDTO ()
     {
+        out.println(marker);
         return productService.getProductDTO();
     }
 
-    @GetMapping("/getVarProducts")
+    @GetMapping("/getAllVarProducts")
     public List<ProductDTO> getVarProducts ()
     {
+        out.println(marker);
         var varProducts = productService.getAllProducts();
-        new ShowManager().inConsole.showThis(varProducts);
+        new DTOManager().showThis((ProductDTO) varProducts);
         return varProducts;
+    }
+
+    @GetMapping("/getOneVarProduct")
+    public ProductDTO getOneVarProduct ()
+    {
+        var varProduct = productService.getProductDTO();
+        new DTOManager().showThis(varProduct);
+        return varProduct;
     }
 }
