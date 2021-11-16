d8fcfa304361562300f314d23e6de7d0f97befc2
Rodion <rodionchik17@gmail.com>
2021-11-16 17:35:54 +0200

    refactoring CategoryDTO

--- src/main/java/company/business/DTO/components/CategoryDTO.java
+++ src/main/java/company/business/DTO/components/CategoryDTO.java
@@ -1,5 +1,6 @@
 package company.business.DTO.components;
 
-public enum CategoryDTO {
+public enum CategoryDTO
+{
     PROCESSOR, MOTHERBOARD, RAM, HARD_DISK_DRIVE, SOLID_STATE_DRIVE, BOX, POWER_SUPPLY, GRAPHICS_CARD, EXPANSION_CARD
 }
