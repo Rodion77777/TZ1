87574ee70d7699e6a2aac48a7b67487de7b3f225
Rodion <rodionchik17@gmail.com>
2021-11-16 17:32:12 +0200

    refactoring ManufacturedRepository

--- src/main/java/company/domain/repository/ManufacturedRepository.java
+++ src/main/java/company/domain/repository/ManufacturedRepository.java
@@ -1,15 +1,9 @@
 package company.domain.repository;
 
-import company.domain.entity.CountryEntity;
 import company.domain.entity.ManufacturerEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface ManufacturedRepository {
-    ManufacturerEntity findById (long id);
-    void save (ManufacturerEntity manufacturerEntity);
-    void update (ManufacturerEntity manufacturerEntity);
-    void delete (ManufacturerEntity manufacturerEntity);
-    List<ManufacturerEntity> findAll ();
-    List<ManufacturerEntity> findByCountryEntity(CountryEntity countryEntity);
-}
+@Repository
+public interface ManufacturedRepository extends CrudRepository<ManufacturerEntity, Long>
+{}
