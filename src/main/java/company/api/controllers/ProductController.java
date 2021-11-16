69b35ff9ddcdcdc7a330c98dcef4aff2336ce9d5
Rodion <rodionchik17@gmail.com>
2021-11-16 17:35:31 +0200

    refactoring ProductController

--- src/main/java/company/api/controllers/ProductController.java
+++ src/main/java/company/api/controllers/ProductController.java
@@ -3,49 +3,43 @@ package company.api.controllers;
 import company.business.DTO.ProductDTO;
 import company.business.services.ProductService;
 import company.domain.managers.DTOManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
-import static java.lang.System.out;
 
 @RestController
 public class ProductController {
-    private ProductService productService;
-    private String marker = "*\n".repeat(50);
 
-    public ProductController (ProductService productService)
-    {
-        this.productService = productService;
-    }
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/getAllProducts")
     public List<ProductDTO> getAllProducts ()
     {
-        out.println(marker);
         return productService.getAllProducts();
     }
 
 
     @GetMapping("/getProductDTO")
-    public ProductDTO getProductDTO ()
+    public ProductDTO getProductDTO (@RequestParam long id)
     {
-        out.println(marker);
-        return productService.getProductDTO();
+        return productService.getProductDTOById(id);
     }
 
     @GetMapping("/getAllVarProducts")
     public List<ProductDTO> getVarProducts ()
     {
-        out.println(marker);
         var varProducts = productService.getAllProducts();
         new DTOManager().showThis((ProductDTO) varProducts);
         return varProducts;
     }
 
     @GetMapping("/getOneVarProduct")
-    public ProductDTO getOneVarProduct ()
+    public ProductDTO getOneVarProduct (@RequestParam long id)
     {
-        var varProduct = productService.getProductDTO();
+        var varProduct = productService.getProductDTOById (id);
         new DTOManager().showThis(varProduct);
         return varProduct;
     }
