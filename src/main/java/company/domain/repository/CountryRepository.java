62535653b3ca22b06a6c58b097e12a58faedbbc6
Rodion <rodionchik17@gmail.com>
2021-11-16 17:31:47 +0200

    refactoring CountryRepository

--- src/main/java/company/domain/repository/CountryRepository.java
+++ src/main/java/company/domain/repository/CountryRepository.java
@@ -1,14 +1,9 @@
 package company.domain.repository;
 
-
 import company.domain.entity.CountryEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface CountryRepository {
-    CountryEntity findById (long id);
-    void save (CountryEntity countryEntity);
-    void update (CountryEntity countryEntity);
-    void delete (CountryEntity countryEntity);
-    List<CountryEntity> findAll ();
-}
+@Repository
+public interface CountryRepository extends CrudRepository<CountryEntity, Long>
+{}
